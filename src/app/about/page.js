ec3aefd11e06f9ff3c03efbaec864e4cf73ac7ef
faatima abdirahmaan ali <115091686+Siyonaa30@users.noreply.github.com>
2023-08-10 13:03:40 +0300

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,7 +5,7 @@ function about() {
     <div className="max-w-6xl mx-auto space-y-5 p-12">
       <h1 className="text-2xl font-medium text-amber-600 p-4">About</h1>
       <p className="pl-4">
-        IMDb is the world's most popular and authoritative source for movie, TV
+        IMDb is the world&apos;s most popular and authoritative source for movie, TV
         and celebrity content. Find ratings and reviews for the newest movie and
         TV shows. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
         Dicta fugit molestiae iure quas est, laborum necessitatibus mollitia.
